61166bc21cbd2f28b8dc333b3562796a7071cb3b
Max Desiatov <m_desiatov@apple.com>
2026-04-13 18:08:46 +0100

    Test split dwarf with `swift-driver` E2E

--- test/DebugInfo/split-dwarf-wasm.swift
+++ test/DebugInfo/split-dwarf-wasm.swift
@@ -1,15 +1,15 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-swift-frontend -primary-file %s -emit-object -g -parse-stdlib \
-// RUN:   -split-dwarf-output %t/test.dwo -o %t/test.o
-// RUN: %llvm-dwarfdump -v --debug-info %t/test.o | %FileCheck --check-prefix=OBJ %s
-// RUN: %llvm-dwarfdump -v %t/test.dwo | %FileCheck --check-prefix=DWO %s
-// RUN: llvm-objdump -h %t/test.dwo | %FileCheck --check-prefix=DWO-SECTIONS %s
+// RUN: cd %t && %target-swiftc_driver -c %s \
+// RUN:   -g -enable-split-dwarf -parse-stdlib -module-name test
+// RUN: %llvm-dwarfdump -v --debug-info %t/split-dwarf-wasm.o | %FileCheck --check-prefix=OBJ %s
+// RUN: %llvm-dwarfdump -v %t/split-dwarf-wasm.dwo | %FileCheck --check-prefix=DWO %s
+// RUN: llvm-objdump -h %t/split-dwarf-wasm.dwo | %FileCheck --check-prefix=DWO-SECTIONS %s
 
 // REQUIRES: CPU=wasm32
 
 /// The object should contain only a skeleton CU (no full debug info).
 // OBJ: DW_TAG_compile_unit
-// OBJ:   DW_AT_GNU_dwo_name{{.*}}"test.dwo"
+// OBJ:   DW_AT_GNU_dwo_name{{.*}}"split-dwarf-wasm.dwo"
 // OBJ:   DW_AT_GNU_dwo_id
 // OBJ-NOT: DW_TAG_subprogram
 
